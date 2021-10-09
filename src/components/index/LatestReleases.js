cb6858bf32f99a794ad2f8fccdbef129091540d0
thommccarthy <mccarttj@gmail.com>
2021-10-09 19:05:14 -0400

    newest albums loaded dynamically

--- src/components/index/LatestReleases.js
+++ src/components/index/LatestReleases.js
@@ -1,9 +1,56 @@
 import React from "react"
 import * as latestReleasesStyles from "./LatestReleases.module.css"
 import { graphql, useStaticQuery, Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 
 const LatestReleases = () => {
+  const releaseData = useStaticQuery(graphql`
+    query {
+      releases: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/content/release/" } }
+        limit: 2
+      ) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              albumArt {
+                childImageSharp {
+                  gatsbyImageData
+                }
+              }
+              artistName
+              title
+              releaseDate(formatString: "")
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(releaseData.releases)
+  const newestAlbum = {
+    albumTitle: releaseData.releases.edges[0].node.frontmatter.title,
+    albumArtist: releaseData.releases.edges[0].node.frontmatter.artistName,
+    albumCover:
+      releaseData.releases.edges[0].node.frontmatter.albumArt.childImageSharp
+        .gatsbyImageData,
+    slug: releaseData.releases.edges[0].node.fields.slug,
+  }
+
+  const secondNewestAlbum = {
+    albumTitle: releaseData.releases.edges[1].node.frontmatter.title,
+    albumArtist: releaseData.releases.edges[1].node.frontmatter.artistName,
+    albumCover:
+      releaseData.releases.edges[1].node.frontmatter.albumArt.childImageSharp
+        .gatsbyImageData,
+    slug: releaseData.releases.edges[1].node.fields.slug,
+  }
+
   return (
     <div className={latestReleasesStyles.wrapper}>
       <div className={latestReleasesStyles.headerWrapper}>
@@ -11,27 +58,38 @@ const LatestReleases = () => {
       </div>
       {/* seasick */}
       <div className={latestReleasesStyles.albumsWrapper}>
-        <div className={latestReleasesStyles.singleAlbumWrapper}>
-          <StaticImage
+        {/* Newest Album */}
+        <Link
+          to={`/releases${newestAlbum.slug}`}
+          className={latestReleasesStyles.singleAlbumWrapper}
+        >
+          <GatsbyImage
             className={latestReleasesStyles.albumCover}
-            src="../../images/index/seasick-cover.jpg"
+            image={newestAlbum.albumCover}
           />
-          <h3 className={latestReleasesStyles.albumArtist}>Thom McCarthy</h3>
+          <h3 className={latestReleasesStyles.albumArtist}>
+            {newestAlbum.albumArtist[0]}
+          </h3>
           <p className={latestReleasesStyles.albumTitle}>
-            seasick wall of circuits
+            {newestAlbum.albumTitle}
           </p>
-        </div>
-        {/* Bottomless */}
-        <div className={latestReleasesStyles.singleAlbumWrapper}>
-          <StaticImage
+        </Link>
+        {/* second newest album */}
+        <Link
+          to={`/releases${secondNewestAlbum.slug}`}
+          className={latestReleasesStyles.singleAlbumWrapper}
+        >
+          <GatsbyImage
             className={latestReleasesStyles.albumCover}
-            src="../../images/index/bottomless-cover.jpg"
+            image={secondNewestAlbum.albumCover}
           />
-          <h3 className={latestReleasesStyles.albumArtist}>Rocky Kaminski</h3>
+          <h3 className={latestReleasesStyles.albumArtist}>
+            {secondNewestAlbum.albumArtist[0]}
+          </h3>
           <p className={latestReleasesStyles.albumTitle}>
-            bottomless pit/the cannibal song
+            {secondNewestAlbum.albumTitle}
           </p>
-        </div>
+        </Link>
       </div>
     </div>
   )
