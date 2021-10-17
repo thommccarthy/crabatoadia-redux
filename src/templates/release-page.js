9fa4b754a2ea153ff62d5ab5b8e8416cc6f92357
thommccarthy <mccarttj@gmail.com>
2021-10-17 17:22:39 -0400

    update yml

--- src/templates/release-page.js
+++ src/templates/release-page.js
@@ -5,28 +5,43 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import * as releasePageStyles from "./release-page.module.css"
 
 export default function ReleasePage({ data }) {
-  console.log(data)
-
   return (
     <div>
       <Layout>
         <div className={releasePageStyles.wrapper}>
-          <GatsbyImage
-            className={releasePageStyles.albumArt}
-            image={
-              data.markdownRemark.frontmatter.albumArt.childImageSharp
-                .gatsbyImageData
-            }
-          />
-          <iframe
-            style={{ border: `0`, width: `400px`, height: `274px` }}
-            src={`${data.markdownRemark.frontmatter.bandcampEmbed}`}
-            seamless
-          >
-            <a href={data.markdownRemark.frontmatter.bandcampLink}>
-              Seasick Wall of Circuits by Thom McCarthy
-            </a>
-          </iframe>
+          <h1 className={releasePageStyles.artistName}>
+            {data.markdownRemark.frontmatter.artistName[0]}
+          </h1>
+          <h3 className={releasePageStyles.albumTitle}>
+            {data.markdownRemark.frontmatter.title}
+          </h3>
+          {/* cover and bandcamp embed */}
+          <div className={releasePageStyles.coverAndBandcampWrapper}>
+            {/* <GatsbyImage
+              className={releasePageStyles.albumArt}
+              image={
+                data.markdownRemark.frontmatter.albumArt.childImageSharp
+                  .gatsbyImageData
+              }
+            /> */}
+            <div className={releasePageStyles.bandcampWrapper}>
+              <iframe
+                style={{ border: `0`, width: `390px`, height: `510px` }}
+                src={`${data.markdownRemark.frontmatter.bandcampEmbed}`}
+                seamless
+              >
+                <a href={data.markdownRemark.frontmatter.bandcampLink}></a>
+              </iframe>
+            </div>
+            <div class={releasePageStyles.creditsWrapper}>
+              <div
+                class={releasePageStyles.credits}
+                dangerouslySetInnerHTML={{
+                  __html: data.markdownRemark.frontmatter.description,
+                }}
+              ></div>
+            </div>
+          </div>
         </div>
       </Layout>
     </div>
@@ -37,11 +52,13 @@ export const query = graphql`
   query ReleaseQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       id
+
       frontmatter {
         artistName
         bandcampLink
         bandcampEmbed
         date
+        description
         albumArt {
           childImageSharp {
             gatsbyImageData
