c9e2fae5fc3008efd71fbdd90d73b4a52e99fbc4
thommccarthy <mccarttj@gmail.com>
2021-11-02 17:18:18 -0400

    form stuff

--- src/components/footer/ContactForm.js
+++ src/components/footer/ContactForm.js
@@ -1,7 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import * as contactFormStyles from "./ContactForm.module.css"
 
 const ContactForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+
+  const resetForm = () => {
+    setFormState({
+      name: "",
+      email: "",
+      message: "",
+    })
+  }
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&")
+  }
+
+  const handleChange = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formState }),
+    }).catch(error => alert(error))
+    resetForm()
+    e.preventDefault()
+  }
+
   return (
     <div className={contactFormStyles.wrapper}>
       <div className={contactFormStyles.bookingWrapper}>
@@ -18,18 +55,42 @@ const ContactForm = () => {
           <form
             id="contact"
             method="POST"
+            name="contact"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}
             className={contactFormStyles.formWrapper}
           >
             <input type="hidden" name="bot-field" />
-            <input type="hidden" name="contact" value="contact"></input>
-            <label className={contactFormStyles.contactUs} for="name">
-              Contact Us
-            </label>
-            <input type="text" id="name" name="name" placeholder="name*" />
-            <input type="email" id="email" name="email" placeholder="email*" />
-            <textarea type="text" placeholder="message*" />
+            <input type="hidden" name="form-name" value="contact"></input>
+            <h1 className={contactFormStyles.contactUs}>Contact Us</h1>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="name*"
+              onChange={handleChange}
+              value={formState.name}
+              required
+            />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email*"
+              onChange={handleChange}
+              value={formState.email}
+              required
+            />
+            <textarea
+              id="message"
+              name="message"
+              type="text"
+              placeholder="message*"
+              onChange={handleChange}
+              value={formState.message}
+              required
+            />
             <button className={contactFormStyles.submitButton} type="submit">
               Submit
             </button>
