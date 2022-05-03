b4ceb3a3ed06d71b8dde1712620c59e473ec6290
thommccarthy <mccarttj@gmail.com>
2022-05-03 12:47:37 -0400

    meta tag needed closing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,18 @@ import React from "react"
 import LatestReleases from "../components/index/LatestReleases"
 import * as indexStyles from "./index.module.css"
 import WhatIs from "../components/index/WhatIs"
+import { Helmet } from "react-helmet"
 
 export default function Home() {
   return (
     <>
+      <Helmet>
+        <title>Home | Crabatoadia Records</title>
+        <meta
+          name="description"
+          content="Official website of Crabatoadia Records, a Philadelphia-based independent record label."
+        />
+      </Helmet>
       <h1 className={indexStyles.visuallyHiddenHeader}>Crabatoadia Home</h1>
       <LatestReleases />
       <WhatIs />
